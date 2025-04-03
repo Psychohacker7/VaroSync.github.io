95393f14022dedde474f28f9febf83a8d4106be9
Psychohacker7 <benkhaleq101@gmail.com>
2025-04-03 04:52:18 -0400

    Setup Github Pages

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Solutions from './pages/Solutions';
 import Company from './pages/Company';
